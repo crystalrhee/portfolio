59849c8246018cfdb01a7f2bb3e92337c8748819
Crystal Rhee <crhee@hubspot.com>
2022-09-15 13:04:12 -0400

    Revert "Merge branch 'master' into gh-pages"
    This reverts commit 87274739d57f332d94f5de37b51d86926cfeaeab, reversing
    changes made to 006fc3da30991750538bdd5d4aa86d4dd59ed061.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Crystal's Portfolio Website
-
-Hello! This is the code for my portfolio website, which can be found [here](https://crystalrhee.github.io).
--- src/images/certifications/index.js
+++
@@ -1,5 +0,0 @@
-export const Neu = require('./neu.png');
-export const Tsinghua = require('./tsinghua.png');
-export const Yonsei = require('./yonsei.png');
-export const Bloomberg = require('./bloomberg.jpg');
-export const Tips = require('./tips.jpeg');
